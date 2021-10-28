681ede9235dfddd4a02d88d879e3c013ca1ea9e7
Cagatay <cagataybayramgonen@gmail.com>
2021-10-28 16:51:22 +0300

    first commit

---
+++ src/test/java/tests/day13/C01_Pages.java
@@ -0,0 +1,33 @@
+package tests.day13;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utilities.TestBase;
+
+public class C01_Pages extends TestBase {
+    // amazon sayfasina gidip
+    // nutella için arama
+    // sonuc sayisini yazisi print
+    // sonuc yazisinin nutella icerdigini test edin.
+    @Test
+    public void test(){
+        driver.get("https://www.amazon.com/");
+        WebElement searchBoxElement = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBoxElement.sendKeys("Nutella", Keys.ENTER);
+        WebElement resultTextElement = driver.findElement(By.xpath("//div[@class='a-section a-spacing-small a-spacing-top-small']"));
+        System.out.println(resultTextElement.getText());
+        Assert.assertTrue(resultTextElement.getText().contains("Nutella"),"Nutella icermiyor");
+    }
+    @Test
+    public void testPOM(){
+        driver.get("https://www.amazon.com/");
+        AmazonPage amazonPage = new AmazonPage(driver);
+        amazonPage.seachBox.sendKeys("Nutella"+ Keys.ENTER);
+        System.out.println(amazonPage.resultText.getText());
+        Assert.assertTrue(amazonPage.resultText.getText().contains("Nutella"),"Nutella icermiyor");
+    }
+}
