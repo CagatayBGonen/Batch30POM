0b4f3a7b10c1b9ecd8d3828194e4fce5b198f34e
Cagatay <cagataybayramgonen@gmail.com>
2021-10-31 17:58:32 +0300

    E2ETest

--- src/test/java/pages/QAConcortPage.java
+++ src/test/java/pages/QAConcortPage.java
@@ -4,13 +4,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import utilities.ConfigurationReader;
 import utilities.Driver;
 
 public class QAConcortPage {
     public WebDriver driver;
-    public QAConcortPage(){
-        PageFactory.initElements(Driver.getDriver(),this);
+
+    public QAConcortPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
     }
+
     @FindBy(linkText = "Log in")
     public WebElement loginButton;
     @FindBy(id = "UserName")
@@ -21,4 +24,32 @@ public class QAConcortPage {
     public WebElement submitBttn;
     @FindBy(id = "divMessageResult")
     public WebElement resultBox;
+    @FindBy(id = "menuHotels")
+    public WebElement menuHotel;
+    @FindBy(partialLinkText = "Hotel List")
+    public WebElement hotelList;
+    @FindBy(xpath = "//span[text()='Add Hotel ']")
+    public WebElement addHotelBttn;
+    @FindBy(id = "Code")
+    public WebElement codeBox;
+    @FindBy(id = "Name")
+    public WebElement nameBox;
+    @FindBy(id = "IDGroup")
+    public WebElement idGroupDropdownMenu;
+    @FindBy(xpath = "//div[@class='bootbox-body']")
+    public WebElement hotelSavedBox;
+    @FindBy(xpath = "//button[@data-bb-handler='ok']")
+    public WebElement okBttn;
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement saveBttn;
+
+
+    public void qaConcortHotelAdminLogin() {
+        Driver.getDriver().get(ConfigurationReader.getProperty("CHQAUrl"));
+        QAConcortPage qaConcortPage = new QAConcortPage();
+        qaConcortPage.loginButton.click();
+        qaConcortPage.usernameBox.sendKeys(ConfigurationReader.getProperty("CHvalidUsername"));
+        qaConcortPage.passwordBox.sendKeys(ConfigurationReader.getProperty("CHvalidPassword"));
+        qaConcortPage.submitBttn.click();
+    }
 }
---
+++ src/test/java/tests/day15/C02_E2ETest.java
@@ -0,0 +1,59 @@
+package tests.day15;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.QAConcortPage;
+import utilities.Driver;
+
+public class C02_E2ETest {
+    /*
+    1. Tests packagenin altına class olusturun: D17_CreateHotel
+2. Bir metod olusturun: createHotel
+3. http://qa-environment.koalaresorthotels.com adresine git.
+4. Username textbox ve password metin kutularını locate edin ve asagidaki verileri    girin.
+	a. Username : manager
+	b. Password  : Manager1!
+5. Login butonuna tıklayın.
+6. Hotel Management/Hotel List menusunden ADD HOTEL butonuna tiklayin
+7. Açılan sayfadaki tüm metin kutularına istediğiniz verileri girin.
+8. Save butonuna tıklayın.
+9. “Hotel was inserted successfully” textinin göründüğünü test edin.
+10. OK butonuna tıklayın.
+
+     */
+    @Test
+    public void test() throws InterruptedException {
+        Faker faker = new Faker();
+        QAConcortPage  qaConcortPage = new QAConcortPage();
+        qaConcortPage.qaConcortHotelAdminLogin();
+        qaConcortPage.menuHotel.click();
+        qaConcortPage.hotelList.click();
+        qaConcortPage.addHotelBttn.click();
+        Actions actions = new Actions(Driver.getDriver());
+
+        actions.click(qaConcortPage.codeBox)
+                .sendKeys(faker.address().zipCode())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.name().name())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.address().fullAddress())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.phoneNumber().cellPhone())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.internet().emailAddress())
+                .perform();
+        Select select = new Select(qaConcortPage.idGroupDropdownMenu);
+        select.selectByIndex(1);
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        Thread.sleep(2000);
+        qaConcortPage.saveBttn.click();
+        Thread.sleep(2000);
+        Assert.assertTrue(qaConcortPage.hotelSavedBox.isDisplayed(),"save text gozukmuyor");
+        qaConcortPage.okBttn.click();
+        Driver.closeDriver();
+    }
+}
