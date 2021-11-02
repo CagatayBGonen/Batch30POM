408693321d0ab5dd97b5532d84e29f2bf0ad39cf
Cagatay <cagataybayramgonen@gmail.com>
2021-11-02 10:47:10 +0300

    xml groups

--- src/test/java/tests/day13/C03_Pages02.java
+++ src/test/java/tests/day13/C03_Pages02.java
@@ -1,5 +1,6 @@
 package tests.day13;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 import pages.AmazonPage;
@@ -17,7 +18,7 @@ public class C03_Pages02 extends TestBase {
         AmazonPage amazonPage = new AmazonPage(driver);
         Select select = new Select(amazonPage.dropdownMenu);
         select.selectByVisibleText("Books");
-        amazonPage.seachBox.sendKeys("Java");
+        amazonPage.seachBox.sendKeys("Java", Keys.ENTER);
         System.out.println(amazonPage.resultText.getText());
 
     }
