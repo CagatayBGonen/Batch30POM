7bce8b891b723c97292fe98af5c8625ec06f0292
Cagatay <cagataybayramgonen@gmail.com>
2021-11-11 22:12:37 +0300

    cross browser01

--- src/test/java/crossBrowser/PositiveLoginTest.java
+++ src/test/java/crossBrowser/PositiveLoginTest.java
@@ -1,21 +1,17 @@
-package tests.smokeTest;
+package crossBrowser;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.QAConcortPage;
 import utilities.ConfigurationReader;
 import utilities.Driver;
-import utilities.TestBaseRapor;
 
-public class PositiveLoginTest extends TestBaseRapor {
+public class PositiveLoginTest {
     @Test
     public void positiveLoginTest(){
-        extentTest=extentReports.createTest("positive login test","siteye basarili girme testi");
         Driver.getDriver().get(ConfigurationReader.getProperty("CHQAUrl"));
-        extentTest.info("Concort Hotel sitesine gidildi");
         QAConcortPage qaConcortPage = new QAConcortPage();
         qaConcortPage.loginButton.click();
-        extentTest.info("login tusuna basildi");
         qaConcortPage.usernameBox.sendKeys(ConfigurationReader.getProperty("CHvalidUsername"));
         qaConcortPage.passwordBox.sendKeys(ConfigurationReader.getProperty("CHvalidPassword"));
         qaConcortPage.submitBttn.click();
---
+++ src/test/java/utilities/DriverCross.java
@@ -0,0 +1,62 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class DriverCross {
+    private DriverCross() {
+
+    }
+
+    private static WebDriver driver;
+    // driver'i baska classlardan sadece Driver class ismi ile cagırabilmek icin Static yaptik
+    // henuz bu driver ile ilgili ayarlar yapmadigim icin baska classlar bunu yanlislikla kullanmasin diye
+    // erisimi private yaptik (sadece bu classin kullanimina acik yaptik)
+
+    public static WebDriver getDriver(String browser) {
+        if (driver == null) {
+            browser = browser == null ? ConfigurationReader.getProperty("browser") : browser;
+            // browser tanimlamayi unutursak, yukaridaki kod garanti olarak islev gorur.
+            switch (browser) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                case "opera":
+                    WebDriverManager.operadriver().setup();
+                    driver = new OperaDriver();
+                    break;
+                case "edge":
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver();
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+            }
+
+
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
+        driver = null;
+    }
+}
